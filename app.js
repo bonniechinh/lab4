d35cfc855c27508dc79bdb7c9f39c944da33008e
vagrant <vagrant@precise32.(none)>
2015-01-29 21:50:40 +0000

    node.js lab

--- app.js
+++ app.js
@@ -13,6 +13,8 @@ var index = require('./routes/index');
 // var user = require('./routes/user');
 
 var app = express();
+var index = require('./routes/index');
+var project = require('./routes/project');
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -36,6 +38,7 @@ if ('development' == app.get('env')) {
 
 // Add routes here
 app.get('/', index.view);
+app.get('/project/:name', project.viewProject);
 // Example route
 // app.get('/users', user.list);
 
